ba2fc5c294cf321e0b4fa2d103db2f573d408fca
VincentWC_Huang <VincentWC_Huang@asus.com>
2018-01-16 09:31:42 +0800

    Calculator

---
+++ Calculator.Text/CalculatorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Calculator.Text
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            ICalculator cal = new Calculator();
+            int result = cal.Add(5, 6);
+            Assert.That(result, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void SubTest()
+        {
+            ICalculator cal = new Calculator();
+            int result = cal.Sub(5, 6);
+            Assert.That(result, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void MulTest()
+        {
+            ICalculator cal = new Calculator();
+            int result = cal.Mul(5, 6);
+            Assert.That(result, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void DivTest()
+        {
+            ICalculator cal = new Calculator();
+            int result = cal.Div(5, 6);
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
